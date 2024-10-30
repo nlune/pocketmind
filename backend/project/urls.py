56dbcc1f1b505b0c1129b78cbb4881cb0efc43d6
Lune <ln@appl-e.fritz.box>
2024-10-30 12:44:53 +0100

    added swagger docs

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -18,11 +18,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 BASE_URL = "backend/api/"
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PocketMind Backend API",
+        default_version='v1',
+        description="By Constructor Team Munich",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="learn@propulsionacademy.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,  # Set to False restrict access to protected endpoints
+    permission_classes=(permissions.AllowAny,), # Permissions for docs access
+)
 
 urlpatterns = [
     path(BASE_URL + 'admin/', admin.site.urls),
+    path(BASE_URL + 'docs/', schema_view.with_ui('swagger', cache_timeout=0)),
+
     path(BASE_URL + 'auth/', include('registration.urls')),
     path(BASE_URL + 'transactions/', include('expense.urls')),
 ]

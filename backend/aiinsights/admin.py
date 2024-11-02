66f24d7bddfff37de5ae04f77d4af28a023b12f1
Zukhra_Abdulaeva <zukhra.abdulaeva@outlook.de>
2024-11-02 15:55:33 +0100

    add endpoint dashboard

---
+++ backend/aiinsights/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from dashboard.models import AIInsight
+
+# Register your models here.
+admin.site.register(AIInsight)
---
+++ backend/test.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+import openai
+
+print("python-dotenv und openai erfolgreich importiert!")

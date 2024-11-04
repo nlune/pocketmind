3ef54c7eac5be59724981f21c7578df35b4d05d4
dnice <denizahmetoglu89@gmail.com>
2024-11-04 15:36:23 +0100

    added generated pages and routes

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -3,6 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import HomePage from "../pages/Home";
 import AudioInputPage from "../pages/AudioInputPage";
+import CamInputPage from "../pages/CamInputPage.jsx";
+import LoginPage from "../pages/Login.jsx";
+import RegistrationPage from "../pages/Registration.jsx";
+import ProfilePage from "../pages/Profile.jsx";
+import NotFoundPage from "../pages/NotFound.jsx";
+import PasswordResetPage from "../pages/PasswordReset.jsx";
+
 
 export default function MainRoutes() {
 
@@ -13,7 +20,12 @@ export default function MainRoutes() {
         <Route element={<Layout/>}>
             <Route path='/' element={<HomePage/>}/>
             <Route path='/audio-input/' element={<AudioInputPage/>}/>
-            
+            <Route path='/camera-input/' element={<CamInputPage/>}/>
+            <Route path='/login/' element={<LoginPage/>}/>
+            <Route path='/registration/' element={<RegistrationPage/>}/>
+            <Route path='/passwordreset/' element={<PasswordResetPage/>}/>
+            <Route path='/profile/' element={<ProfilePage/>}/>
+            <Route path='/notfound/' element={<NotFoundPage/>}/>
         </Route >
         </Routes>
         </>

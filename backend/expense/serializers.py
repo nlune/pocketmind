4e910ff0c2c16411b98c38531685dd9d98fc084b
V-Hildebrandt <viktor.hildebrandt@googlemail.com>
2024-11-05 18:21:12 +0100

    added view for reports url and endpoints tested
    another riddle:
    Voiceless it cries,
    Wingless flutters,
    Toothless bites,
    Mouthless mutters.

--- backend/expense/serializers.py
+++ backend/expense/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
-
 from expense.models import Expense
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Expense
         fields = "__all__"
+
+    def get_category(self, obj):
+        if obj.category:
+            return {
+                "id": obj.category.id,
+                "name": obj.category.name
+            }
+        return None
+
+    def get_user(self, obj):
+        if obj.user:
+            return {
+                "id": obj.user.id,
+                "username": obj.user.username
+            }
+        return None
--- backend/expense/urls.py
+++ backend/expense/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from expense.views import GetExpenseFromInput, ListAddExpenses, AddExpense
+from expense.views import GetExpenseFromInput, ListAddExpenses, AddExpense, ReportsView
 
 urlpatterns = [
     path("", ListAddExpenses.as_view(), name="expense-list"),
     path("add-by-user/", AddExpense.as_view(), name="expense-add"),
-    path("get-via-input/", GetExpenseFromInput.as_view())
+    path("get-via-input/", GetExpenseFromInput.as_view()),
+    path("reports/", ReportsView.as_view(), name="expense-report"),
 ]

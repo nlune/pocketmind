91fe5dd0fdd3bb43f4b5b78c64629239a8f53de2
Zukhra_Abdulaeva <zukhra.abdulaeva@outlook.de>
2024-11-06 21:57:36 +0100

    User_Setting None-Error solved

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from .views import user_settings_view
 
 urlpatterns = [
-    path('settings/', user_settings_view, name='user_settings'),
+    path('user/settings/', user_settings_view, name='user-settings'),
+    #path('settings/', user_settings_view, name='user_settings'),
 ]

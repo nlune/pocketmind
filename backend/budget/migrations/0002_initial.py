8a888c18ec0680f82cf0d4fe881aea395aef2332
V-Hildebrandt <viktor.hildebrandt@googlemail.com>
2024-11-07 14:19:57 +0100

    resolve merge conflicts and applied reformat code for lint

--- backend/budget/migrations/0002_initial.py
+++ backend/budget/migrations/0002_initial.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -17,6 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='budget',
             name='category',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='budget', to='category.category'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='budget',
+                                    to='category.category'),
         ),
     ]
--- backend/settings/admin.py
+++ backend/settings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+
 from .models import UserSettings
 
 
 @admin.register(UserSettings)
 class UserSettingsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'goals', 'pocket_enabled')   # Customize what you want to display
+    list_display = ('user', 'goals', 'pocket_enabled')  # Customize what you want to display

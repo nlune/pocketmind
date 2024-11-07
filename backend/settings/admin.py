587c739af0648b0f1bdc85d414ca53bb31bf5922
Zukhra_Abdulaeva <zukhra.abdulaeva@outlook.de>
2024-11-07 01:17:37 +0100

    User_Setting None-Error solved

--- backend/settings/admin.py
+++ backend/settings/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import UserSettings
 
 #admin.site.register(UserSettings)
-
 @admin.register(UserSettings)
 class UserSettingsAdmin(admin.ModelAdmin):
     list_display = ('user',                     

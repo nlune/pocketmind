f9ba7ecf4bc9065e83c564f826e0685b5c38b0b6
Zukhra_Abdulaeva <zukhra.abdulaeva@outlook.de>
2024-11-07 01:31:52 +0100

    User_Setting None-Error solved

--- backend/settings/admin.py
+++ backend/settings/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import UserSettings
 
-#admin.site.register(UserSettings)
+
 @admin.register(UserSettings)
 class UserSettingsAdmin(admin.ModelAdmin):
-    list_display = ('user',                     
-                    'goals','pocket_enabled')   # Customize what you want to display
+    list_display = ('user', 'goals', 'pocket_enabled')   # Customize what you want to display
--- backend/settings/views.py
+++ backend/settings/views.py
@@ -8,24 +8,25 @@ from .serializers import UserSettingsSerializer
 
 logger = logging.getLogger(__name__)
 
+
 @api_view(['GET', 'PATCH'])
 def user_settings_view(request):
     try:
         user = request.user
-        
+
         # Ensure the user is authenticated
         if not user.is_authenticated:
             logger.error("Unauthorized access attempt by anonymous user.")
             return Response({"detail": "Authentication required"}, status=status.HTTP_401_UNAUTHORIZED)
-        
+
         # Try to get UserSettings, or create if it doesn't exist
         user_settings, created = UserSettings.objects.get_or_create(user=user)
-        
+
         if request.method == 'GET':
             # Serialize user settings data
             user_serializer = UserSettingsSerializer(user_settings)
             return Response(user_serializer.data, status=status.HTTP_200_OK)
-        
+
         if request.method == 'PATCH':
             # Handle updates to user settings
             user_serializer = UserSettingsSerializer(user_settings, data=request.data, partial=True)
@@ -35,7 +36,7 @@ def user_settings_view(request):
             else:
                 logger.error(f"Validation error: {user_serializer.errors}")
                 return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     except Exception as e:
         logger.exception(f"Error fetching or creating user settings: {str(e)}")
         return Response({"detail": "Internal server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

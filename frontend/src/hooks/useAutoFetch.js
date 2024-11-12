6076ed9723ecc11f8c01b8378cd75c0dce4f13a7
Zukhra_Abdulaeva <zukhra.abdulaeva@outlook.de>
2024-11-12 16:03:11 +0100

    saved changes from master branch

--- frontend/src/hooks/useAutoFetch.js
+++ frontend/src/hooks/useAutoFetch.js
@@ -1,6 +1,7 @@
 import {useEffect} from 'react'
 import useApiRequest from './useAPI'
 
+
 const useAutoFetch = (method, url, requestData, trigger) => {
     const token = localStorage.getItem('auth-token')
     const {sendRequest, data, error, loading} = useApiRequest()

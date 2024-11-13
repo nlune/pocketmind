8dc66d3671653bb033fab112e0b90179ced44855
V-Hildebrandt <viktor.hildebrandt@googlemail.com>
2024-11-13 20:53:27 +0100

    lint my good old friend

--- backend/expense/admin.py
+++ backend/expense/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Expense
 
 
@@ -6,5 +7,6 @@ class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('description', 'amount', 'created', 'user', 'category', 'is_recurring', 'start_date', 'end_date')
     fields = ('description', 'amount', 'created', 'user', 'category', 'is_recurring', 'start_date', 'end_date')
     readonly_fields = ('created',)
+
 
 admin.site.register(Expense, ExpenseAdmin)
